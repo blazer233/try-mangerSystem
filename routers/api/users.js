1d7eb5a1540dc55cf0d04768c35bd7d19d56f6d4
Song <52182701+blazer233@users.noreply.github.com>
2019-11-28 10:45:29 +0800

    Update users.js

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
-const bcrypt = require('bcrypt')
+const crypto = require("crypto");
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 const User = require('../../models/Users')
@@ -21,24 +21,19 @@ router.post('/register', (req, res) => {
 					r: 'pg',
 					d: 'mm'
 				});
+				let password = req.body.password;
+				let md5 = crypto.createHash("md5");
+				let newPas = md5.update(password).digest("hex");
 				const newUser = new User({
 					username: req.body.username,
 					email: req.body.email,
 					avatar,
-					password: req.body.password,
+					password: newPas,
 					identity: req.body.identity
 				})
-				bcrypt.genSalt(10, function(err, salt) {
-					bcrypt.hash(newUser.password, salt, (err, hash) => {
-						if (err) {
-							throw err
-						}
-						newUser.password = hash
-						newUser.save()
-							.then(user => res.json(user))
-							.catch(err => console.log(err))
-					});
-				});
+				newUser.save()
+					.then(user => res.json(user))
+					.catch(err => console.log(err))
 			}
 		})
 })
@@ -46,37 +41,38 @@ router.post('/register', (req, res) => {
 //登陆
 router.post('/login', (req, res) => {
 	const email = req.body.email
-	const password = req.body.password
+	let password = req.body.password;
+	let md5 = crypto.createHash("md5");
+	let newPas = md5.update(password).digest("hex");
+	console.log(newPas)
 	User.findOne({
-			email
-		})
-		.then(user => {
-			if (!user) {
-				return res.status(404).json("用户不存在")
+		username: req.body.username,
+		password: newPas
+	}).then(user => {
+		if (!user) { //token
+			return res.status(404).json("用户不存在")
+		} else {
+			const rule = { //token规则,包含哪些字段
+				id: user.id,
+				username: user.username,
+				avatar: user.avatar,
+				identity: user.identity
 			}
-			bcrypt.compare(password, user.password)
-				.then(isMatch => {
-					if (isMatch) {
-						const rule = {
-							id: user.id,
-							username: user.username,
-							avatar: user.avatar,
-							identity: user.identity
-						}
-						jwt.sign(rule, "secret", {
-							expiresIn: 3600
-						}, (err, token) => {
-							if (err) throw err
-							res.json({
-								success: true,
-								token: "Bearer " + token
-							})
-						})
-					} else {
-						return res.status(404).json("密码错误")
-					}
+			//jwt.sign('规则','加密名字','过期时间','箭头函数')
+			jwt.sign(rule, "secret", {
+				expiresIn: 3600 //一小时后过期
+			}, (err, token) => {
+				if (err) throw err
+				res.json({
+					success: true,
+					token: "Bearer " + token
 				})
-		})
+			})
+		}
+	}).catch(err => {
+		console.log("错误是:" + err)
+	})
+
 })
 
 //passport-jwt验证token
