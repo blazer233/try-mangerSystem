e9de5317eae8d714c2bba7254b71b64f92d3f173
Song <52182701+blazer233@users.noreply.github.com>
2020-05-14 20:23:41 +0800

    Update Week.js

--- models/Week.js
+++ models/Week.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose')
-const newSchema=require('../schema/week')
+const Schema = mongoose.Schema
+const newSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    find_id: {
+        type: String,
+        required: true
+    },
+    region: {
+        type: String,
+        required: true
+    },
+    date0: {
+        type: String,
+        required: true
+    },
+    data1: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    delivery: {
+        type: Boolean,
+    }
+})
 
-module.exports=mongoose.model('Week',newSchema)
+module.exports = Week = mongoose.model('Week', newSchema)
