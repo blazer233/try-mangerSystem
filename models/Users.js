67b6855c5bd25ad7099772fc0dd077dfb531573c
Song <52182701+blazer233@users.noreply.github.com>
2020-05-14 20:11:43 +0800

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,30 +1,4 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const newSchema=require('../schema/user')
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  avatar: {
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  identity: {
-    type: String,
-    required: true
-  }
-})
-
-module.exports = User = mongoose.model('users',UserSchema)
+module.exports=mongoose.model('User',newSchema)
