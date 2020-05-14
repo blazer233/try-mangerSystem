2487b50b052c8c60acdf45a2fd82ce2294fba56b
Song <52182701+blazer233@users.noreply.github.com>
2020-05-14 20:07:38 +0800

    Update users.js

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,88 +1,98 @@
-const express = require('express')
-const router = express.Router()
-const gravatar = require('gravatar')
-const crypto = require("crypto");
+const Router = require('koa-router')
+const router = new Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/Users.js')
 const jwt = require('jsonwebtoken')
-const passport = require('passport')
-const User = require('../../models/Users')
+const gravatar = require('gravatar')
+const passport = require('koa-passport')
 
-
-//注册
-router.post('/register', (req, res) => {
-	User.findOne({
-			email: req.body.email
-		})
-		.then((user) => {
-			if (user) {
-				return res.status(400).json("邮箱已经被注册")
-			} else {
-				const avatar = gravatar.url(req.body.email, {
-					s: '200',
-					r: 'pg',
-					d: 'mm'
-				});
-				let password = req.body.password;
-				let md5 = crypto.createHash("md5");
-				let newPas = md5.update(password).digest("hex");
-				const newUser = new User({
-					username: req.body.username,
-					email: req.body.email,
-					avatar,
-					password: newPas,
-					identity: req.body.identity
-				})
-				newUser.save()
-					.then(user => res.json(user))
-					.catch(err => console.log(err))
-			}
-		})
+router.get('/all', async ctx => {
+    let find_ = await User.find()
+    ctx.body = find_ ? find_ : ''
 })
 
-//登陆
-router.post('/login', (req, res) => {
-	const email = req.body.email
-	let password = req.body.password;
-	let md5 = crypto.createHash("md5");
-	let newPas = md5.update(password).digest("hex");
-	User.findOne({
-		email,
-		password: newPas
-	}).then(user => {
-		if (user) { //token
-			const rule = { //token规则,包含哪些字段
-				id: user.id,
-				username: user.username,
-				avatar: user.avatar,
-				identity: user.identity
-			}
-			//jwt.sign('规则','加密名字','过期时间','箭头函数')
-			jwt.sign(rule, "secret", {
-				expiresIn: 3600 //一小时后过期
-			}, (err, token) => {
-				if (err) throw err
-				res.json({
-					success: true,
-					token: "Bearer " + token
-				})
-			})
-		} else {
-			return res.status(404).json("用户不存在")
-		}
-	}).catch(err => {
-		console.log("错误是:" + err)
-	})
+
+router.post('/register', async ctx => {
+    try {
+        let find_ = await User.findOne({
+            email: ctx.request.body.email
+        })
+        if (find_) {
+            ctx.body = {
+                info: '邮箱已注册',
+                success: false
+            }
+        } else {
+            const avatar = gravatar.url(ctx.request.body.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(ctx.request.body.password, salt);
+            let newUse = new User({
+                avatar,
+                username: ctx.request.body.username,
+                password: hash,
+                email: ctx.request.body.email,
+                identity:ctx.request.body.identity
+            })
+
+            let result = await newUse.save()
+            if (result) {
+                ctx.body = result
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
 
 })
 
-//passport-jwt验证token
-router.get('/current', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	res.json({
-		id: req.user.id,
-		username: req.user.username,
-		email: req.user.email,
-		identity: req.user.identity
-	})
+router.post('/login', async ctx => {
+    let find_ = await User.findOne({
+        email: ctx.request.body.email
+    })
+    if (!find_) {
+        ctx.body = {
+            info: '用户不存在',
+            success: false
+        }
+    } else {
+        let hash_ = bcrypt.compareSync(ctx.request.body.password, find_.password);
+        if (!hash_) {
+            ctx.body = {
+                info: '密码错误',
+                success: false
+            }
+        } else {
+            let pay = {
+                id: find_.id,
+                name: find_.username,
+                date: find_.date,
+                avatar: find_.avatar,
+                identity:find_.identity
+            }
+            let token = jwt.sign(pay, 'art', {
+                expiresIn: 60 * 60 * 60 * 2
+            });
+            ctx.body = {
+                success: true,
+                token: `Bearer ${token}`
+            }
+        }
+    }
 })
-module.exports = router
+
+router.get('/pre', passport.authenticate('jwt', {
+    session: false
+}), async ctx => {
+    ctx.body = {
+        id: ctx.state.user.id,
+        name: ctx.state.user.username,
+        email: ctx.state.user.email,
+        date: ctx.state.user.date,
+    }
+})
+
+module.exports = router.routes()
