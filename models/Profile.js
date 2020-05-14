509d70bde83449bcd3d2e5d1591eb6534f79e844
Song <52182701+blazer233@users.noreply.github.com>
2020-05-14 20:12:10 +0800

    Update Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -1,33 +1,4 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const newSchema=require('../schema/profile')
 
-const ProfileSchema = new Schema({
-  type: {
-    type: String
-  },
-  describe: {
-    type: String
-  },
-  income: {
-    type: String,
-    required: true
-  },
-  expend: {
-    type: String,
-    required: true
-  },
-  cash: {
-    type: String,
-    required: true
-  },
-  remark: {
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-})
-
-module.exports = Profile = mongoose.model('profile',ProfileSchema)
-
+module.exports=mongoose.model('Profile',newSchema)
