679400400ebac0a70965009c1d04655af94893da
Song <52182701+blazer233@users.noreply.github.com>
2020-05-14 20:07:59 +0800

    Update profiles.js

--- routers/api/profiles.js
+++ routers/api/profiles.js
@@ -1,91 +1,88 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-const Profile = require('../../models/Profile')
+const Router = require('koa-router')
+const router = new Router()
+const passport = require('koa-passport')
+const Profile = require('../models/Profiles')
 
+//插入数据
+router.get('/toall', async ctx => {
+    try {
+        let dd = {
+            type: '折旧费',
+            describe: '设备折旧损坏',
+            income: '5115',
+            expend: '900',
+            cash: '18000',
+            remark: '900',
+            id: ''
+        }
+        let cc = new Array(1000).fill(dd)
+        let result = await Profile.insertMany(cc)
+        result ? ctx.body = result : ''
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 //创建信息
 router.post('/add', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	let writeone = new Profile({
-		type: req.body.type,
-		describe: req.body.describe,
-		income: req.body.income,
-		expend: req.body.expend,
-		cash: req.body.cash,
-		remark: req.body.remark
-	})
+    session: false
+}), async ctx => {
+    try {
+        let writeone = new Profile(ctx.request.body)
+        let result = await writeone.save()
+        ctx.body = result ? result : ''
+    } catch (error) {
+        console.log(error)
+    }
 
-	writeone.save().then(profile => {
-		res.json(profile)
-	})
 })
 
 //获得全部信息
 router.get('/', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	Profile.find()
-		.then(profile => {
-			if (!profile) {
-				return res.status(404).json("没有任何内容")
-			}
-			res.json(profile)
-		})
-		.catch(err => {
-			res.status(404).json(err)
-		})
+    session: false
+}), async ctx => {
+    try {
+        let result = await Profile.find()
+        ctx.body = result ? result : {
+            info: '没有任何内容'
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 //获取单个信息
 router.get('/:id', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	Profile.findOne({
-			_id: req.params.id
-		})
-		.then(profile => {
-			if (!profile) {
-				return res.status(404).json("没有任何内容")
-			}
-			res.json(profile)
-		})
-		.catch(err => {
-			res.status(404).json(err)
-		})
+    session: false
+}), async ctx => {
+    let result = await Profile.findOne({
+        _id: ctx.params.id
+    })
+    ctx.body = result ? result : {
+        info: '没有任何内容'
+    }
 })
 
 //编辑信息
 router.post('/edit/:id', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	Profile.updateOne({
-		_id: req.params.id
-	}, {
-		type: req.body.type,
-		describe: req.body.describe,
-		income: req.body.income,
-		expend: req.body.expend,
-		cash: req.body.cash,
-		remark: req.body.remark
-	}).then(profile => {
-		res.json(profile)
-	})
+    session: false
+}), async ctx => {
+    let result = await Profile.updateOne({
+        _id: ctx.params.id
+    }, ctx.request.body)
+    ctx.body = result ? result : ''
 })
 
 
 // 删除信息
 router.delete('/delete/:id', passport.authenticate('jwt', {
-	session: false
-}), (req, res) => {
-	Profile.deleteOne({
-		_id: req.params.id
-	}).then(result => {
-		if (result) {
-			res.json(result)
-		}
-	}).catch(err => res.status(404).json("删除失败"))
+    session: false
+}), async ctx => {
+    let result = await Profile.deleteOne({
+        _id: ctx.params.id
+    })
+    ctx.body = result ? result : ''
 })
 
-module.exports = router
+module.exports = router.routes()
