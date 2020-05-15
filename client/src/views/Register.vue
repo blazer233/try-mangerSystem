ecabe5723ac6f636a54cbb2a545976d42d1b7375
Song <52182701+blazer233@users.noreply.github.com>
2020-05-15 18:21:24 +0800

    Update Register.vue

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -3,7 +3,13 @@
     <section class="form_container">
       <div class="manage_tip">
         <span class="title">后台管理系统</span>
-        <el-form :model="registerUser" :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+        <el-form
+          :model="registerUser"
+          :rules="rules"
+          ref="registerForm"
+          label-width="80px"
+          class="registerForm"
+        >
           <el-form-item label="用户名" prop="username">
             <el-input v-model="registerUser.username" placeholder="请输入用户名"></el-input>
           </el-form-item>
@@ -21,10 +27,16 @@
               <el-option label="管理员" value="manager"></el-option>
               <el-option label="员工" value="employee"></el-option>
             </el-select>
-          </el-form-item>
+          </el-form-item> 
           <el-form-item>
             <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注册</el-button>
           </el-form-item>
+          <div class="tiparea">
+            <p>
+              返回
+              <router-link to="/login">登录</router-link>
+            </p>
+          </div>
         </el-form>
       </div>
     </section>
@@ -38,62 +50,72 @@ export default {
   data() {
     var validatePass2 = (rule, value, callback) => {
       if (value !== this.registerUser.password) {
-        callback(new Error('两次输入密码不一致!'));
+        callback(new Error("两次输入密码不一致!"));
       } else {
         callback();
       }
     };
     return {
       registerUser: {
-        username: '',
-        email: '',
-        password: '',
-        password2: '',
-        identity: ''
+        username: "",
+        email: "",
+        password: "",
+        password2: "",
+        identity: ""
       },
       rules: {
         username: [
-            { required: true, message: "用户名不能为空", trigger: "blur" },
-            { min: 2, max: 15, message: "长度在2-15字符之间", trigger: "blur" }
-          ],
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+          { min: 2, max: 15, message: "长度在2-15字符之间", trigger: "blur" }
+        ],
         email: [
-          { required: true, type: "email", message: "邮箱格式不正确", trigger: "blur" }
+          {
+            required: true,
+            type: "email",
+            message: "邮箱格式不正确",
+            trigger: "blur"
+          }
         ],
         password: [
-          { required: true, message: "密码不能为空", trigger: "blur"  },
-          { min: 6, max: 15, message: "密码长度在6-15字符之间", trigger: "blur" }
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "密码长度在6-15字符之间",
+            trigger: "blur"
+          }
         ],
         password2: [
-          { required: true, message: "密码不能为空", trigger: "blur"  },
-          { min: 6, max: 15, message: "密码长度在6-15字符之间", trigger: "blur" },
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "密码长度在6-15字符之间",
+            trigger: "blur"
+          },
           { validator: validatePass2, trigger: "blur" }
         ],
         identity: []
       }
-    }
+    };
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
-        if (valid) {
-          this.$axios
-            .post("/api/users/register", this.registerUser)
-            .then(res => {
-              // 注册成功
-              this.$message({
-                message: "注册成功！",
-                type: "success"
-              });
-              this.$router.push("/login");
-            });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
+        valid ? this.register_() : "";
       });
+    },
+    async register_() {
+      let res = await this.$axios.post(
+        "/api/users/register",
+        this.registerUser
+      );
+      res.data.info
+        ? this.$message({ message: res.data.info, type: "error" })
+        : this.$message({ message: "注册成功！", type: "success" });
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -101,15 +123,16 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
-  background: url(../assets/bg.jpg) no-repeat center center;
+  background: url(../assets/timg.jpg) no-repeat center center;
   background-size: 100% 100%;
 }
 .form_container {
   width: 370px;
   height: 210px;
   position: absolute;
-  top: 10%;
-  left: 34%;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -80%);
   padding: 25px;
   border-radius: 5px;
   text-align: center;
@@ -118,7 +141,7 @@ export default {
   font-family: "Microsoft YaHei";
   font-weight: bold;
   font-size: 26px;
-  color: #fff;
+  color: currentColor;
 }
 .registerForm {
   margin-top: 20px;
@@ -127,7 +150,14 @@ export default {
   border-radius: 5px;
   box-shadow: 0px 5px 10px #cccc;
 }
-
+.tiparea {
+  text-align: right;
+  font-size: 12px;
+  color: #333;
+}
+.tiparea p a {
+  color: #409eff;
+}
 .submit_btn {
   width: 100%;
 }
