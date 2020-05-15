562c986542797c0062bd5d8d800a061a779611ff
Song <52182701+blazer233@users.noreply.github.com>
2020-05-15 18:20:26 +0800

    Update Fundlist.vue

--- client/src/views/Fundlist.vue
+++ client/src/views/Fundlist.vue
@@ -1,143 +1,80 @@
 <template>
-    <div class="fillcontain">
-        <div>
-            <el-form
-                :inline="true"
-                ref="search_data" 
-                :model='search_data' >
-                <el-form-item label="投标时间筛选:">
-                    <el-date-picker
-                        v-model="search_data.startTime"
-                        type="datetime"
-                        placeholder="选择开始时间">
-                    </el-date-picker> --
-                    <el-date-picker
-                        v-model="search_data.endTime"
-                        type="datetime"
-                        placeholder="选择结束时间">
-                    </el-date-picker>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" size ="small" icon="search" @click='onScreeoutMoney()'>筛选</el-button>
-                </el-form-item>
-                 <el-form-item class="btnRight">
-                    <el-button type="primary" size ="small" icon="view" @click='onAddMoney()'>添加</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
-        <div class="table_container">
-            <el-table
-                v-if="tableData.length > 0"
-                :data='tableData'
-                max-height="450"
-                border
-                :default-sort = "{prop: 'date', order: 'descending'}"
-                style="width: 100%">
-                 <el-table-column
-                    type="index"
-                    label="序号"
-                    align='center'
-                    width="70">
-                </el-table-column>
-                <el-table-column
-                    prop="date"
-                    label="创建时间"
-                    align='center'
-                    width="250"
-                    sortable>
-                    <template slot-scope="scope">
-                        <el-icon name="time"></el-icon>
-                        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                    prop="type"
-                    label="收支类型"
-                    align='center'
-                    width="150">
-                </el-table-column>
-                <el-table-column
-                    prop="describe"
-                    label="收支描述"
-                    align='center'
-                    width="180">
-                </el-table-column>
-                <el-table-column
-                    prop="income"
-                    label="收入"
-                    align='center'
-                    width="170"> 
-                    <template slot-scope="scope">  
-                        <span style="color:#00d053">+ {{ scope.row.income }}</span>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                    prop="expend"
-                    label="支出"
-                    align='center'
-                    width="170">
-                    <template slot-scope="scope">  
-                        <span style="color:#f56767">- {{ scope.row.expend }}</span>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                    prop="cash"
-                    label="账户现金"
-                    align='center'
-                    width="170">
-                    <template slot-scope="scope">  
-                        <span style="color:#4db3ff">{{ scope.row.cash }}</span>
-                    </template>
-                </el-table-column>
-                 <el-table-column
-                    prop="remark"
-                    label="备注"
-                    align='center'
-                    width="220">
-                </el-table-column>
-                <el-table-column
-                    prop="operation"
-                    align='center'
-                    label="操作"
-                    fixed="right"
-                    width="180">
-                    <template slot-scope='scope'>
-                        <el-button 
-                            type="warning" 
-                            icon='edit' 
-                            size="small"
-                            @click='onEditMoney(scope.row)'
-                        >编辑</el-button>
-                        <el-button 
-                            type="danger" 
-                            icon='delete' 
-                            size="small"
-                            @click='onDeleteMoney(scope.row,scope.$index)'
-                        >删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-            <!-- 分页 -->
-             <el-row>
-                <el-col :span="24">
-                    <div class="pagination">
-                        <el-pagination
-                            v-if='paginations.total > 0'
-                            :page-sizes="paginations.page_sizes"
-                            :page-size="paginations.page_size"
-                            :layout="paginations.layout"
-                            :total="paginations.total"
-                            :current-page.sync='paginations.page_index'
-                            @current-change='handleCurrentChange'
-                            @size-change='handleSizeChange'>
-                        </el-pagination>
-                    </div>
-                </el-col>
-            </el-row>
-        </div>
-        <!-- 弹框页面 -->
-        <DialogFound :dialog='dialog' :form='form' @update="getProfile"></DialogFound>
+  <div class="fillcontain">
+    <div>
+      <el-form :inline="true" ref="search_data" :model="search_data">
+        <el-form-item label="投标时间筛选:">
+          <el-date-picker v-model="search_data.startTime" type="datetime" placeholder="选择开始时间"></el-date-picker>--
+          <el-date-picker v-model="search_data.endTime" type="datetime" placeholder="选择结束时间"></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="small" icon="search" @click="onScreeoutMoney()">筛选</el-button>
+        </el-form-item>
+        <el-form-item class="btnRight">
+          <el-button type="primary" size="small" icon="view" @click="onAddMoney()">添加</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+    <div class="table_container">
+      <el-table
+        v-if="tableData.length > 0"
+        :data="tableData"
+        max-height="850"
+        border
+        fit
+        highlight-current-row
+        :default-sort="{prop: 'date', order: 'descending'}"
+        style="width: 100%"
+        :cell-style="{padding:'9px 0'}"
+      >
+        <el-table-column
+          v-for="(item, index) in tableLabel"
+          :type="item.type"
+          :key="index"
+          :align="item.align"
+          :prop="item.prop"
+          :width="item.width"
+          :label="item.label"
+        ></el-table-column>
+        <el-table-column
+          prop="operation"
+          align="center"
+          label="操作"
+          fixed="right"
+          width="180"
+          v-if="show"
+        >
+          <template slot-scope="scope">
+            <el-button type="warning" icon="edit" size="small" @click="onEditMoney(scope.row)">编辑</el-button>
+            <el-button
+              type="danger"
+              icon="delete"
+              size="small"
+              @click="onDeleteMoney(scope.row,scope.$index)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-row>
+        <el-col :span="24">
+          <div class="pagination">
+            <el-pagination
+              v-if="paginations.total > 0"
+              :page-sizes="paginations.page_sizes"
+              :page-size="paginations.page_size"
+              :layout="paginations.layout"
+              :total="paginations.total"
+              :current-page.sync="paginations.page_index"
+              @current-change="handleCurrentChange"
+              @size-change="handleSizeChange"
+            ></el-pagination>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 弹框页面 -->
+    <DialogFound :dialog="dialog" :form="form" @update="getProfile"></DialogFound>
+  </div>
 </template>
 
 <script>
@@ -147,6 +84,57 @@ export default {
   name: "fundlist",
   data() {
     return {
+      tableLabel: [
+        {
+          type: "index",
+          label: "序号",
+          align: "center",
+          width: "70"
+        },
+        {
+          prop: "date",
+          label: "创建时间",
+          align: "center",
+          width: "250"
+        },
+        {
+          prop: "type",
+          label: "收支类型",
+          align: "center",
+          width: "150"
+        },
+        {
+          prop: "describe",
+          label: "收支描述",
+          align: "center",
+          width: "180"
+        },
+        {
+          prop: "income",
+          label: "收入",
+          align: "center",
+          width: "170"
+        },
+        {
+          prop: "expend",
+          label: "支出",
+          align: "center",
+          width: "170"
+        },
+        {
+          prop: "cash",
+          label: "账户现金",
+          align: "center",
+          width: "170"
+        },
+        {
+          prop: "remark",
+          label: "备注",
+          align: "center",
+          width: "220"
+        }
+      ],
+      show: false,
       tableData: [],
       allTableData: [],
       filterTableData: [],
@@ -155,21 +143,13 @@ export default {
         title: "",
         option: "edit"
       },
-      form: {
-        type: "",
-        describe: "",
-        income: "",
-        expend: "",
-        cash: "",
-        remark: "",
-        id: ""
-      },
+      form: {},
       //需要给分页组件传的信息
       paginations: {
         page_index: 1, // 当前位于哪页
         total: 0, // 总数
-        page_size: 5, // 1页显示多少条
-        page_sizes: [5, 10, 15, 20], //每页显示多少条
+        page_size: 15, // 1页显示多少条
+        page_sizes: [15, 20, 25, 30], //每页显示多少条
         layout: "total, sizes, prev, pager, next, jumper" // 翻页属性
       },
       search_data: {
@@ -183,25 +163,19 @@ export default {
   },
   created() {
     this.getProfile();
+    this.show = this.$store.getters.user.identity == "manager" ? true : false;
   },
   methods: {
-    getProfile() {
-      // 获取表格数据
-      this.$axios("/api/profiles").then(res => {
-        // this.tableData = res.data;
-        this.allTableData = res.data;
-        this.filterTableData = res.data;
-        // 设置分页数据
-        this.setPaginations();
-      });
+    // 获取表格数据
+    async getProfile() {
+      let res = await this.$axios("/api/profiles");
+      this.allTableData = res.data;
+      this.filterTableData = res.data;
+      this.setPaginations();
     },
     onEditMoney(row) {
       // 编辑
-      this.dialog = {
-        show: true,
-        title: "修改资金信息",
-        option: "edit"
-      };
+      this.dialog = { show: true, title: "修改资金信息", option: "edit" };
       this.form = {
         type: row.type,
         describe: row.describe,
@@ -212,12 +186,13 @@ export default {
         id: row._id
       };
     },
-    onDeleteMoney(row, index) {
+    async onDeleteMoney(row, index) {
       // 删除
-      this.$axios.delete(`/api/profiles/delete/${row._id}`).then(res => {
+      let res = await this.$axios.delete(`/api/profiles/delete/${row._id}`);
+      if (res.status == 200) {
         this.$message("删除成功");
         this.getProfile();
-      });
+      }
     },
     onAddMoney() {
       // 添加
@@ -237,7 +212,7 @@ export default {
       };
     },
     handleCurrentChange(page) {
-      // 当前页
+      // 取到的页数
       let sortnum = this.paginations.page_size * (page - 1);
       let table = this.allTableData.filter((item, index) => {
         return index >= sortnum;
@@ -248,7 +223,7 @@ export default {
       });
     },
     handleSizeChange(page_size) {
-      // 切换size
+      // 切换的一页数目
       this.paginations.page_index = 1;
       this.paginations.page_size = page_size;
       this.tableData = this.allTableData.filter((item, index) => {
@@ -259,7 +234,7 @@ export default {
       // 总页数
       this.paginations.total = this.allTableData.length;
       this.paginations.page_index = 1;
-      this.paginations.page_size = 5;
+      this.paginations.page_size = 15;
       // 设置默认分页数据
       this.tableData = this.allTableData.filter((item, index) => {
         return index < this.paginations.page_size;
