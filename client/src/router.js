85d030c548cc9f0e988c1b819ff637a1f41cd71f
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:28:20 +0800

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,21 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from './views/index.vue'
-import Register from './views/Register.vue'
 import Notfound from './views/404.vue'
-import Login from './views/Login.vue'
+import LoginReg from './views/LoginReg.vue'
 import Home from './views/Home.vue'
 import Infoshow from './views/Infoshow.vue'
 import Fundlist from './views/Fundlist.vue'
 import workerlist from './views/workerlist.vue'
-import workerlist_week from './views/workerlist_week.vue'
 Vue.use(Router)
 
-const router =  new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       redirect: '/index'
     },
@@ -23,40 +20,57 @@ const router =  new Router({
       path: '/index',
       name: 'index',
       component: Index,
-      children: [
-        { path: "", component: Home },
-        { path: "/home", name: "home", component: Home },
-        { path: "/infoshow", name: "infoshow", component: Infoshow },
-        { path: "/fundlist", name: "fundlist", component: Fundlist },
-        { path: "/workerlist_week", name: "workerlist_week", component: workerlist_week },
-        { path: "/workerlist", name: "workerlist", component: workerlist }
+      children: [{
+          path: "",
+          component: Home
+        },
+        {
+          path: "/home",
+          name: "home",
+          component: Home
+        },
+        {
+          path: "/infoshow",
+          name: "infoshow",
+          component: Infoshow
+        },
+        {
+          path: "/fundlist",
+          name: "fundlist",
+          component: Fundlist
+        },
+        {
+          path: "/workerlist",
+          name: "workerlist",
+          component: workerlist
+        },
+        {
+          path: "/workerlist",
+          name: "workerlist",
+          component: workerlist
+        }
 
       ]
     },
     {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '/Notfound', 
+      path: '/Notfound',
       name: '/404',
       component: Notfound
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: LoginReg
     }
   ]
 })
 
 //路由守卫
-router.beforeEach((to,from,next) => {
+router.beforeEach((to, from, next) => {
   const isLogin = localStorage.eleToken ? true : false
-  if(to.path == "/login" || to.path == "/register"){
+  if (to.path == "/login" || to.path == "/register") {
     next()
-  }else{
+  } else {
     isLogin ? next() : next("/login")
   }
 })
