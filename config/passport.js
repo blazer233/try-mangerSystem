8ea81158d3dd2ac903dcc8c691caef04c53f8b5f
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:26:10 +0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,15 @@
-//const mongoose = require('mongoose')
-//const User = mongoose.model('users')  此处如果这么引入会报错 需要从server.js文件中先引入users模型
-const User = require('../models/Users')
 const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const opts = {}
+const User = require("../models/Users")
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret'
+opts.secretOrKey = 'art';
 
-module.exports =  passport => {
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    //console.log(jwt_payload)
-    User.findById(jwt_payload.id)
-      .then(user=>{
-        if(user){
-          return done(null,user)
-        }
-        return done(null,false)
-      }) 
-      .catch(err=>console.log(err))
-  }));
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+        let result = await User.findOne({
+            _id: jwt_payload.id
+        })
+        return result ? done(null, result) : done(null, false)
+    }));
 }
