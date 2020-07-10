43bd526634b695889ad26ca0859355d3afee6582
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:35:45 +0800

    Update Infoshow.vue

--- client/src/views/Infoshow.vue
+++ client/src/views/Infoshow.vue
@@ -3,18 +3,18 @@
     <el-row type="flex" class="row-bg" justify="center">
       <el-col :span="8">
         <div class="user">
-          <img :src="user.avatar" class="avatar" alt="">
+          <img :src="user.avatar" />
         </div>
       </el-col>
       <el-col :span="16">
         <div class="userinfo">
           <div class="user-item">
             <i class="el-icon-info"></i>
-            <span>{{user.name}}</span> 
+            <span>{{user.name}}</span>
           </div>
           <div class="user-item">
             <i class="el-icon-setting"></i>
-            <span>{{user.identity == "manager" ? "管理员" : "普通用户"}}</span>
+            <span>{{user.identity? "管理员" : "普通用户"}}</span>
           </div>
           <div class="user-item">
             <i class="el-icon-setting"></i>
@@ -29,12 +29,46 @@
 <script>
 export default {
   name: "infoshow",
-  computed: {
-    user(){ 
-      return this.$store.getters.user
-    }
+  data() {
+    return {
+      dialogVisible: false,
+      imgSrc: "",
+      user: {}
+    };
   },
-}
+  created() {
+    this.user = this.$store.getters.user;
+  },
+  methods: {
+    setImage(e) {
+      const file = e.target.files[0];
+      if (!file.type.includes("image/")) return;
+      const reader = new FileReader();
+      reader.onload = event => {
+        this.dialogVisible = true;
+        this.imgSrc = event.target.result;
+        this.$refs.cropper && this.$refs.cropper.replace(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    },
+    cropImage() {
+      this.user.avatar = this.$refs.cropper.getCroppedCanvas().toDataURL();
+    },
+    cancelCrop() {
+      this.dialogVisible = false;
+      this.user.avatar = this.$store.getters.user.avatar;
+    },
+    imageuploaded(res) {
+      console.log(res);
+    },
+    handleError() {
+      this.$notify.error({
+        title: "上传失败",
+        message: "图片上传接口上传失败，可更改为自己的服务器接口"
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -58,7 +92,6 @@ export default {
   border-radius: 50%;
   position: absolute;
   left: 53%;
-
 }
 .user span {
   display: block;
@@ -77,5 +110,33 @@ export default {
   padding: 26px;
   font-size: 28px;
   color: #333;
+}
+.btn_ {
+  position: relative;
+  width: 100px;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 20px;
+  margin-left: 30px;
+  background-color: #409eff;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 4px;
+  box-sizing: border-box;
+  top: 30rem;
+  left: 20rem;
+}
+.input_ {
+  position: absolute;
+  width: 100px;
+  height: 40px;
+  left: 0;
+  top: 0;
+  opacity: 0;
+  cursor: pointer;
+}
+.cropper-content .cropper {
+  width: auto;
+  height: 300px;
 }
 </style>
