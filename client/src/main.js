1482f6d8c36baf1e9d6875996920fae9dec2f3e1
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:28:32 +0800

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -5,12 +5,35 @@ import axios from './http'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './components/tree/tree.css'
+import './utils/directives/dialogDrag' //弹窗拖动指令
+import './utils/directives/py' //汉转拼音指令
+import {
+  debounce,
+  DateFormat
+} from './utils/utils'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$axios = axios
-
-
+//防抖 
+Vue.directive('debounce', {
+  inserted: function (el, binding) {
+    el.addEventListener("keyup", debounce(binding.value, 200))
+  }
+})
+//节流 
+Vue.directive('throttle', {
+  inserted: function (el, binding) {
+    el.addEventListener("keyup", throttle(binding.value, 200))
+  }
+})
+//时间戳转换
+Vue.directive('DateFormat', {
+  inserted: function (el, binding) {
+    el.innerHTML = DateFormat(binding.value)
+  }
+})
 new Vue({
   router,
   store,
