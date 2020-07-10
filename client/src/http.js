7cbcfd05940af19110067cf9b8cf7826da27651b
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:29:04 +0800

    Update http.js

--- client/src/http.js
+++ client/src/http.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
-import { Loading } from 'element-ui'
-import { Message } from 'element-ui'
+import {
+  Loading,
+  Message
+} from 'element-ui'
 import router from './router'
 let loading
+
 function startLoading() {
   loading = Loading.service({
     lock: true,
@@ -11,7 +14,7 @@ function startLoading() {
   })
 }
 
-function endLoading(){
+function endLoading() {
   loading.close()
 }
 
@@ -20,12 +23,12 @@ function endLoading(){
 axios.interceptors.request.use(config => {
   //加载动画
   startLoading()
-  if(localStorage.eleToken){
+  if (localStorage.eleToken) {
     //设置统一的请求header
     config.headers.Authorization = localStorage.eleToken
   }
   return config
-},error => {
+}, error => {
   return Promise.reject(error)
 })
 
@@ -34,20 +37,25 @@ axios.interceptors.response.use(response => {
   //结束加载动画
   endLoading()
   return response
-},error => {
+}, error => {
   endLoading()
-  Message.error(error,response.data)
   //获取错误状态码
-  const { status } = error.response
-  if(status == 401){
-    Message.error("token失效 请重新登录")
-    //清楚token
-    localStorage.removeItem("eleToken")
-    //跳转到登录页面
-    router.push("/login")
+  if (!error.response) To("请检查服务器");
+  const status = error.response.status
+  if (status == 500) {
+    Message.error("参数有误")
+    return Promise.reject(error)
   }
-
-  return Promise.reject(error) 
+  if (status == 401) To("token失效 请重新登录")
+  return Promise.reject(error)
 })
+
+function To(msg) {
+  Message.error(msg)
+  //清楚token
+  localStorage.removeItem("eleToken")
+  //跳转到登录页面
+  router.push("/login")
+}
 
 export default axios;
