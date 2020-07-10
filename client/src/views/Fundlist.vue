10d1535f8fe6ea97faace8ae94ba5ba53d1fade3
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:34:58 +0800

    Update Fundlist.vue

--- client/src/views/Fundlist.vue
+++ client/src/views/Fundlist.vue
@@ -1,278 +1,207 @@
 <template>
   <div class="fillcontain">
-    <div>
-      <el-form :inline="true" ref="search_data" :model="search_data">
-        <el-form-item label="投标时间筛选:">
-          <el-date-picker v-model="search_data.startTime" type="datetime" placeholder="选择开始时间"></el-date-picker>--
-          <el-date-picker v-model="search_data.endTime" type="datetime" placeholder="选择结束时间"></el-date-picker>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" size="small" icon="search" @click="onScreeoutMoney()">筛选</el-button>
-        </el-form-item>
-        <el-form-item class="btnRight">
-          <el-button type="primary" size="small" icon="view" @click="onAddMoney()">添加</el-button>
-        </el-form-item>
-      </el-form>
+    <div class="center_">
+      <!-- 筛选表单 -->
+      <koa-search
+        :type_="0"
+        :search_all="tableSearch"
+        :controlType="controlType"
+        @getList="getProfile"
+        size="mini"
+        :isinline="true"
+      />
+      <i class="el-icon-edit-outline addOne" @click="onAddMoney" title="添加"></i>
     </div>
-    <div class="table_container">
-      <el-table
-        v-if="tableData.length > 0"
-        :data="tableData"
-        max-height="850"
-        border
-        fit
-        highlight-current-row
-        :default-sort="{prop: 'date', order: 'descending'}"
-        style="width: 100%"
-        :cell-style="{padding:'9px 0'}"
-      >
-        <el-table-column
-          v-for="(item, index) in tableLabel"
-          :type="item.type"
-          :key="index"
-          :align="item.align"
-          :prop="item.prop"
-          :width="item.width"
-          :label="item.label"
-        ></el-table-column>
-        <el-table-column
-          prop="operation"
-          align="center"
-          label="操作"
-          fixed="right"
-          width="180"
-          v-if="show"
-        >
-          <template slot-scope="scope">
-            <el-button type="warning" icon="edit" size="small" @click="onEditMoney(scope.row)">编辑</el-button>
-            <el-button
-              type="danger"
-              icon="delete"
-              size="small"
-              @click="onDeleteMoney(scope.row,scope.$index)"
-            >删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <el-row>
-        <el-col :span="24">
-          <div class="pagination">
-            <el-pagination
-              v-if="paginations.total > 0"
-              :page-sizes="paginations.page_sizes"
-              :page-size="paginations.page_size"
-              :layout="paginations.layout"
-              :total="paginations.total"
-              :current-page.sync="paginations.page_index"
-              @current-change="handleCurrentChange"
-              @size-change="handleSizeChange"
-            ></el-pagination>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
+    <!-- 内容表格 -->
+    <!-- <p v-py="name"></p> -->
+    <input type="text" id="input" v-debounce="find" />
+    <koa-table
+      :tableHeader="tableHeader"
+      :tableData="tableData"
+      :total="total"
+      @pagination_size_change="set_getProfile"
+      @delOne="onDeleteMoney"
+      @editOne="onEditMoney"
+    />
     <!-- 弹框页面 -->
-    <DialogFound :dialog="dialog" :form="form" @update="getProfile"></DialogFound>
+    <el-dialog
+      :visible.sync="dialog.show"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :modal-append-to-body="false"
+      v-dialogDrag
+      class="dialogClass"
+    >
+      <div slot="title" class="dialog_class">
+        <span>{{dialog.title}}</span>
+      </div>
+      <div style="height:500px">
+        <!-- 弹框表单 -->
+        <koa-search
+          ref="search_"
+          :search_all="tableSearch_"
+          :controlType="controlType"
+          :type_="1"
+          @getList="onSubmit"
+          size="mini"
+          :isinline="false"
+        />
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import DialogFound from "../components/DialogFound";
-
+import KoaTable from "@/components/config/table_all";
+import KoaSearch from "@/components/config/from_all";
+import { getProfiles, deleteProfiles, controlProfiles } from "@/api/api";
+import { Config } from "@/components/config/config";
 export default {
   name: "fundlist",
   data() {
     return {
-      tableLabel: [
-        {
-          type: "index",
-          label: "序号",
-          align: "center",
-          width: "70"
-        },
-        {
-          prop: "date",
-          label: "创建时间",
-          align: "center",
-          width: "250"
-        },
-        {
-          prop: "type",
-          label: "收支类型",
-          align: "center",
-          width: "150"
-        },
-        {
-          prop: "describe",
-          label: "收支描述",
-          align: "center",
-          width: "180"
-        },
-        {
-          prop: "income",
-          label: "收入",
-          align: "center",
-          width: "170"
-        },
-        {
-          prop: "expend",
-          label: "支出",
-          align: "center",
-          width: "170"
-        },
-        {
-          prop: "cash",
-          label: "账户现金",
-          align: "center",
-          width: "170"
-        },
-        {
-          prop: "remark",
-          label: "备注",
-          align: "center",
-          width: "220"
-        }
-      ],
-      show: false,
+      name: "注册",
+      tableSearch: Config.indexTable.search,
+      tableSearch_: Config.dialog.search,
+      controlType: Config.controlType,
       tableData: [],
       allTableData: [],
-      filterTableData: [],
+      PageSize: { page_index: 1, page_size: 15 },
+      total: 0,
       dialog: {
         show: false,
         title: "",
         option: "edit"
-      },
-      form: {},
-      //需要给分页组件传的信息
-      paginations: {
-        page_index: 1, // 当前位于哪页
-        total: 0, // 总数
-        page_size: 15, // 1页显示多少条
-        page_sizes: [15, 20, 25, 30], //每页显示多少条
-        layout: "total, sizes, prev, pager, next, jumper" // 翻页属性
-      },
-      search_data: {
-        startTime: "",
-        endTime: ""
       }
     };
   },
+  computed: {
+    tableHeader() {
+      return this.$store.getters.user.identity
+        ? Config.indexTable.table
+        : Config.indexTable.table_employee;
+    }
+  },
   components: {
-    DialogFound
+    KoaTable,
+    KoaSearch
   },
   created() {
     this.getProfile();
-    this.show = this.$store.getters.user.identity == "manager" ? true : false;
   },
   methods: {
+    find() {
+      console.log("ajax发送的数据为： " + input.value);
+    },
+    set_getProfile(obj) {
+      this.PageSize.page_index = obj.page_index;
+      this.PageSize.page_size = obj.page_size;
+      this.getProfile();
+    },
     // 获取表格数据
-    async getProfile() {
-      let res = await this.$axios("/api/profiles");
-      this.allTableData = res.data;
-      this.filterTableData = res.data;
-      this.setPaginations();
+    async getProfile(obj = {}) {
+      let cc = Object.assign(
+        {
+          date1: obj.date ? new Date(obj.date[0]).getTime() : "",
+          date2: obj.date ? new Date(obj.date[1]).getTime() : ""
+        },
+        this.PageSize
+      );
+      let res = await getProfiles(cc);
+      this.tableData = res.data.list;
+      this.total = res.data.total;
     },
-    onEditMoney(row) {
-      // 编辑
-      this.dialog = { show: true, title: "修改资金信息", option: "edit" };
-      this.form = {
-        type: row.type,
-        describe: row.describe,
-        income: row.income,
-        expend: row.expend,
-        cash: row.cash,
-        remark: row.remark,
-        id: row._id
-      };
-    },
-    async onDeleteMoney(row, index) {
-      // 删除
-      let res = await this.$axios.delete(`/api/profiles/delete/${row._id}`);
-      if (res.status == 200) {
-        this.$message("删除成功");
+    async dialog_(obj) {
+      const url = this.dialog.option == "add" ? "add" : `edit/${obj.id}`;
+      let res = await controlProfiles(url, obj);
+      if (res) {
+        this.$message({
+          message: "保存成功！",
+          type: "success"
+        });
+        this.dialog.show = false;
         this.getProfile();
       }
     },
+    // 编辑
+    onEditMoney(row) {
+      this.dialog = {
+        show: true,
+        title: "修改资金信息",
+        option: "edit"
+      };
+      this.$nextTick(function() {
+        this.$refs.search_.search_from = {
+          type: row.type,
+          describe: row.describe,
+          income: row.income,
+          expend: row.expend,
+          cash: row.cash,
+          remark: row.remark,
+          id: row._id,
+          date: new Date().getTime()
+        };
+      });
+    },
+    // 弹出添加添加
     onAddMoney() {
-      // 添加
       this.dialog = {
         show: true,
         title: "添加资金信息",
         option: "add"
       };
-      this.form = {
-        type: "",
-        describe: "",
-        income: "",
-        expend: "",
-        cash: "",
-        remark: "",
-        id: ""
-      };
-    },
-    handleCurrentChange(page) {
-      // 取到的页数
-      let sortnum = this.paginations.page_size * (page - 1);
-      let table = this.allTableData.filter((item, index) => {
-        return index >= sortnum;
-      });
-      // 设置默认分页数据
-      this.tableData = table.filter((item, index) => {
-        return index < this.paginations.page_size;
+      this.$nextTick(function() {
+        this.$refs.search_.search_from = {};
       });
     },
-    handleSizeChange(page_size) {
-      // 切换的一页数目
-      this.paginations.page_index = 1;
-      this.paginations.page_size = page_size;
-      this.tableData = this.allTableData.filter((item, index) => {
-        return index < page_size;
-      });
-    },
-    setPaginations() {
-      // 总页数
-      this.paginations.total = this.allTableData.length;
-      this.paginations.page_index = 1;
-      this.paginations.page_size = 15;
-      // 设置默认分页数据
-      this.tableData = this.allTableData.filter((item, index) => {
-        return index < this.paginations.page_size;
-      });
-    },
-    onScreeoutMoney() {
-      // 筛选
-      if (!this.search_data.startTime || !this.search_data.endTime) {
-        this.$message({
-          type: "warning",
-          message: "请选择时间区间"
-        });
+    // 删除
+    async onDeleteMoney(row) {
+      let res = await deleteProfiles(row._id);
+      if (res.status == 200) {
+        this.$message("删除成功");
         this.getProfile();
-        return;
       }
-      const stime = this.search_data.startTime.getTime();
-      const etime = this.search_data.endTime.getTime();
-      this.allTableData = this.filterTableData.filter(item => {
-        let date = new Date(item.date);
-        let time = date.getTime();
-        return time >= stime && time <= etime;
-      });
-      // 分页数据
-      this.setPaginations();
+    },
+    onSubmit(res) {
+      res.date = new Date().getTime();
+      this.dialog_(res);
     }
   }
 };
-</script>
-<style scoped>
+</script> 
+<style >
+.dialog_class {
+  font-weight: bold;
+  font-size: 20px;
+  font-size: 18px;
+  color: white;
+}
+.dialogClass .el-dialog__body {
+  padding: 0px 20px 120px 20px !important;
+  color: #606266;
+  font-size: 14px;
+}
+.addOne {
+  font-size: 30px;
+  margin-right: 3rem;
+  cursor: pointer;
+}
+.center_ {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .fillcontain {
   width: 100%;
   height: 100%;
   padding: 16px;
   box-sizing: border-box;
 }
+
 .btnRight {
   float: right;
 }
+
 .pagination {
   text-align: right;
   margin-top: 10px;
