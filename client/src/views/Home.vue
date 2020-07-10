42a104f7e59467dc0da933dd24dac8d4ca153f7c
Song <52182701+blazer233@users.noreply.github.com>
2020-07-10 19:35:13 +0800

    Update Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,38 @@
 <template>
-    <div class="home">
-        <div class="container">
-        </div>
+  <div class="home">
+    <div class="leave">部门</div>
+    <div class="container">
+      <tree />
     </div>
+  </div>
 </template>
 
+<script>
+import tree from "@/components/tree/control";
+export default {
+  components: {
+    tree
+  }
+};
+</script>
 <style scoped>
 .home {
   width: 100%;
-  height: 100%;
-  background: url(../assets/showcase.jpg) no-repeat;
+  background: white;
   background-size: 100% 100%;
+}
+.leave {
+  font-size: 25px;
+  border-bottom: 1px solid;
+  margin: 1rem 5rem;
+  padding: 2rem 2rem;
 }
 .container {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-  padding-top: 100px;
-  background-color: rgba(0, 0, 0, 0.7);
+  padding-left: 50px;
+  /* background-color: rgba(0, 0, 0, 0.7); */
   text-align: center;
   color: white;
 }
