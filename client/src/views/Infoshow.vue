6a8ac1b04245cc0503837bcc1d47fc2f53a4229d
Song <52182701+blazer233@users.noreply.github.com>
2020-07-22 18:24:18 +0800

    Update Infoshow.vue

--- client/src/views/Infoshow.vue
+++ client/src/views/Infoshow.vue
@@ -5,6 +5,38 @@
         <div class="user">
           <img :src="user.avatar" />
         </div>
+        <div class="crop-demo">
+          <img :src="cropImg" />
+          <div class="crop-demo-btn">
+            选择图片
+            <input
+              class="crop-input"
+              type="file"
+              name="image"
+              accept="image/*"
+              @change="setImage"
+            />
+          </div>
+        </div>
+
+        <el-dialog title="裁剪图片" :visible.sync="dialogVisible" width="30%">
+          <vue-cropper
+            ref="cropper"
+            :src="imgSrc"
+            :ready="cropImage"
+            :zoom="cropImage"
+            :cropmove="cropImage"
+            style="width:100%;height:300px;"
+          ></vue-cropper>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="cancelCrop">取 消</el-button>
+            <el-button
+              type="primary"
+              @click="dialogVisible = false"
+              @click.prevent="cropperStart()"
+            >确 定</el-button>
+          </span>
+        </el-dialog>
       </el-col>
       <el-col :span="16">
         <div class="userinfo">
@@ -18,7 +50,7 @@
           </div>
           <div class="user-item">
             <i class="el-icon-setting"></i>
-            <span>{{user.date.replace('T',' ').slice(0,-5)}}</span>
+            <span v-DateFormat="user.date"></span>
           </div>
         </div>
       </el-col>
@@ -27,39 +59,69 @@
 </template>
 
 <script>
+import VueCropper from "vue-cropperjs";
+import { controlUser } from "@/api/api";
 export default {
   name: "infoshow",
   data() {
     return {
       dialogVisible: false,
       imgSrc: "",
+      cropImg: "",
       user: {}
     };
   },
   created() {
     this.user = this.$store.getters.user;
   },
+  components: {
+    VueCropper
+  },
   methods: {
     setImage(e) {
-      const file = e.target.files[0];
-      if (!file.type.includes("image/")) return;
-      const reader = new FileReader();
-      reader.onload = event => {
-        this.dialogVisible = true;
-        this.imgSrc = event.target.result;
-        this.$refs.cropper && this.$refs.cropper.replace(event.target.result);
-      };
-      reader.readAsDataURL(file);
+      let res = window.URL.createObjectURL(e.target.files[0]);
+      this.imgSrc = res;
+      this.$refs.cropper && this.$refs.cropper.replace(res);
+      this.dialogVisible = true;
     },
     cropImage() {
-      this.user.avatar = this.$refs.cropper.getCroppedCanvas().toDataURL();
+      this.user.avatar = this.Base64UrlToBlob(
+        this.$refs.cropper.getCroppedCanvas().toDataURL()
+      );
+      console.log(this.user.avatar)
+    },
+    Base64UrlToBlob(urlData) {
+      urlData = urlData.substring(urlData.indexOf(",") + 1);
+      var binary = atob(urlData);
+      var array = [];
+      for (var i = 0, len = binary.length; i < len; i++) {
+        array.push(binary.charCodeAt(i));
+      }
+      return window.URL.createObjectURL(
+        new Blob([new Uint8Array(array)], { type: "image/jpeg" })
+      );
+    },
+    async cropperStart() {
+      console.log(
+        this.Base64UrlToBlob(this.$refs.cropper.getCroppedCanvas().toDataURL())
+      );
+      let obj = {
+        img_: this.Base64UrlToBlob(
+          this.$refs.cropper.getCroppedCanvas().toDataURL()
+        ),
+        _id: this.$store.getters.user.id
+      };
+      let { data } = await controlUser("/userImg", obj);
+      if (data.success) {
+        this.$message({ message: "更换成功！", type: "success" });
+        this.$store.dispatch("setUser_img", obj.img_);
+      } else {
+        this.$message({ message: "更换失败！", type: "error" });
+      }
     },
     cancelCrop() {
       this.dialogVisible = false;
       this.user.avatar = this.$store.getters.user.avatar;
-    },
-    imageuploaded(res) {
-      console.log(res);
     },
     handleError() {
       this.$notify.error({
@@ -89,7 +151,7 @@ export default {
 }
 .user img {
   width: 150px;
-  border-radius: 50%;
+  height: 150px;
   position: absolute;
   left: 53%;
 }
@@ -138,5 +200,28 @@ export default {
 .cropper-content .cropper {
   width: auto;
   height: 300px;
+}
+.crop-demo {
+  display: flex;
+  align-items: flex-end;
+}
+.crop-demo-btn {
+  line-height: 40px;
+  padding: 0 20px;
+  background-color: #409eff;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 4px;
+  box-sizing: border-box;
+  transform: translate(355%, 500px);
+}
+.crop-input {
+  position: absolute;
+  width: 100px;
+  height: 40px;
+  left: 0;
+  top: 0;
+  opacity: 0;
+  cursor: pointer;
 }
 </style>
