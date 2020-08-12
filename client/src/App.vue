51b908f0667668a257a3f9efa52097d4c7c9586c
Song <52182701+blazer233@users.noreply.github.com>
2020-08-12 11:39:24 +0800

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,29 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import jwt_decode from 'jwt-decode'
+import jwt_decode from "jwt-decode";
 export default {
   name: "app",
-  components: {},
   created() {
-    if(localStorage.eleToken){
-      const decoded = jwt_decode(localStorage.eleToken)
-      this.$store.dispatch("setAuthenticated",!this.isEmpty(decoded))
-      this.$store.dispatch("setUser",decoded)
-    }
-  },
-  methods: {
-    isEmpty(value){
-      return (
-        value === undefined || value === null || (typeof value === "object" && Object.keys(value).length === 0) ||
-        (typeof value === "string" && value.trim().length === 0)
-      )
+    if (localStorage.eleToken) {
+      const decoded = jwt_decode(localStorage.eleToken);
+      this.$store.dispatch("setAuthenticated", !this.$isEmpty(decoded));
+      this.$store.dispatch("setUser", decoded); 
     }
   }
-}
+};
 </script>
 
 <style>
 html,
 body,
-#app{
+#app {
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 </style>
